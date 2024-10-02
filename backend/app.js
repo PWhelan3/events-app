cea62e6d3ad17f9b06fc9d58182498aeedc4c614
Peter <pwhelanstudio@gmail.com>
2024-10-02 12:37:18 +0100

    Add routes to backend

--- backend/app.js
+++ backend/app.js
@@ -5,3 +5,7 @@ app.use(cors());
 app.use(express.json());
 
 app.listen(5000, () => console.log('Server running on port 5000'));
+
+
+const meetupRoutes = require('./routes/meetupRoutes');
+app.use('/api/meetups', meetupRoutes);
