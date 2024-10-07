14fcbdeb9a7d2191ab3c32d17775524ebba7cd23
Peter <pwhelanstudio@gmail.com>
2024-10-07 16:49:32 +0100

    Add & install real time updates and socket io

--- backend/app.js
+++ backend/app.js
@@ -9,3 +9,22 @@ app.listen(5000, () => console.log('Server running on port 5000'));
 
 const meetupRoutes = require('./routes/meetupRoutes');
 app.use('/api/meetups', meetupRoutes);
+
+
+
+const http = require('http').Server(app);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: '*',
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log('User connected');
+  socket.on('createMeetup', (meetup) => {
+    socket.broadcast.emit('newMeetup', meetup);
+  });
+});
+
+http.listen(5000, () => console.log('Server running on port 5000'));
+``
